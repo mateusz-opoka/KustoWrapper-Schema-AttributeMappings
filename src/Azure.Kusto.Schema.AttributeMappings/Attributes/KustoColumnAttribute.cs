3d2120cf24715b2aeb7a58e655023bb50120303b
Mateusz Opoka <mateusz@mobiloc.pl>
2020-01-31 19:27:05 +0100

    add KustoColumnAttribute

---
+++ src/Azure.Kusto.Schema.AttributeMappings/Attributes/KustoColumnAttribute.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Azure.Kusto.Schema.AttributeMappings.Attributes
+{
+    [AttributeUsage(AttributeTargets.Property)]
+    public class KustoColumnAttribute : Attribute
+    {
+        public string ColumnName { get; }
+
+        public KustoColumnAttribute() { }
+
+        public KustoColumnAttribute(string columnName)
+        {
+            ColumnName = columnName;
+        }
+    }
+}
